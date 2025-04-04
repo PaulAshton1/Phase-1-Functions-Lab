4f700a8157f860155625084c1194060268cc30e3
PaulAshton1 <ashtonvilla42@gmail.com>
2025-04-04 16:33:38 +0300

    changed

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ function distanceFromHqInBlocks(someValue) {
   }
   
   function calculatesFarePrice(start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
+    const distance = distanceTravelledInFeet(start, destination); 
   
-    if (distance <= 400) {
-      return 0;
-    } else if (distance > 400 && distance <= 2000) {
-      return (distance - 400) * 0.02;  
-    } else if (distance > 2000 && distance <= 2500) {
-      return 25;  
+    if (distance > 2500) {
       return 'cannot travel that far';  
+    } else if (distance > 2000) {
+      return 25; 
+    } else if (distance > 400) {
+      return (distance - 400) * 0.02;  
+      return 0;  
     }
-    }
+  }
+  
